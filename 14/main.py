894eb9b419400683975aed58171a0bec35c8180b
Daniil Semenkov <laindash@mail.ru>
2022-06-05 13:18:06 +0300

    Create main.py

---
+++ 14/main.py
@@ -0,0 +1,14 @@
+def f(i, n):
+    a = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    b = a[i % n]
+    while i >= n:
+        i //= n
+        b += a[i % n]
+    return b[::-1]
+for x in range(2, 32):
+    num = str(31)
+    base = 10
+    t_base = x
+    s = int(num, base)
+    s = f(s, t_base)
+    print(s, t_base)
