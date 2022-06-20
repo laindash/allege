69a78ab9a2d6f6509c459ed91ad432df26a8617f
Daniil Semenkov <87697162+laindash@users.noreply.github.com>
2022-06-20 14:50:48 +0300

    Update 8(часто).py

--- 8/8(часто).py
+++ 8/8(часто).py
@@ -3,5 +3,6 @@ from itertools import product
 c = 0
 for x in product('АПРСУ', repeat=5)):  # ставим буквы по алфавиту
     c += 1
+    x = ''.join(x)
     if x[0] == 'У' and 'АА' not in x:
         print(c)
